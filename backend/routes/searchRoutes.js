d5af2ccde96ab7a097da1f81d0678938025ab268
wacho91 <criskol.71@gmail.com>
2024-07-31 11:37:19 -0500

    complete endpoint search

--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -1,6 +1,15 @@
 import express from "express";
+import { getSearchHistory, removeItemFromSearchHistory, searchMovie, searchPerson, searchTv } from "../controllers/searchController.js";
 
 const router = express.Router();
+
+router.get("/person/:query", searchPerson);
+router.get("/movie/:query", searchMovie);
+router.get("/tv/:query", searchTv);
+
+router.get("/history", getSearchHistory);
+
+router.delete("/history/:id", removeItemFromSearchHistory);
 
 
 export default router;
