1e4b3bc2aa517e1dbb0e169ba8e09b007422ad0b
wacho91 <criskol.71@gmail.com>
2024-07-31 09:52:47 -0500

    Create middleware the protetRoute

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,26 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/authRoutes.js';
 import movieRoutes from './routes/movieRoutes.js';
 import tvRoutes from './routes/tvRoutes.js';
+
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
+import { protectRoute } from "./middleware/protectRoute.js";
+
+
 
 const app = express();
 
 const PORT = ENV_VARS.PORT
 
 app.use(express.json());// will allow use to parse req.body
+app.use(cookieParser)
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes)
+app.use("/api/v1/tv", tvRoutes);
 
 app.listen(PORT, () => {
     console.log("Server started at http://localhost:" + PORT);
