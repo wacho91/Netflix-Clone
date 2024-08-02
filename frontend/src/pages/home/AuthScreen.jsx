2d7436d28806cab7e3b5a857d0c75c340b7be1b8
wacho91 <criskol.71@gmail.com>
2024-08-02 10:56:22 -0500

    3rd section complete

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -46,7 +46,7 @@ const AuthScreen = () => {
           {/*Left side*/}
           <div className="flex-1">
             <div className="flex-1 text-center md:text-left">
-              <h2 className="text-4xl md:text-5xl font-extrabold mb-4">Enjoy on your</h2>
+              <h2 className="text-4xl md:text-5xl font-extrabold mb-4">Enjoy on your Tv</h2>
               <p className="text-lg md:text-xl">
                 Watch on smart TV's, PlayStation, Xbox, Chromecast, Apple, Tv, Blue-ray palyers, and more.
               </p>
@@ -99,6 +99,31 @@ const AuthScreen = () => {
 							Save your favorites easily and always have something to watch.
 						</p>
 					</div>
+        </div>
+      </div>
+
+      {/*Separator*/}
+      <div className="h-2 w-full bg-[#232323]" aria-hidden='true'></div>
+
+      {/*3rd section*/}
+      <div className="py-10 bg-black text-white">
+        <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
+          {/*Left side*/}
+          <div className="flex-1">
+            <div className="flex-1 text-center md:text-left">
+              <h2 className="text-4xl md:text-5xl font-extrabold mb-4">Watch everywhere</h2>
+              <p className="text-lg md:text-xl">
+                Watch on smart TV's, PlayStation, Xbox, Chromecast, Apple, Tv, Blue-ray palyers, and more.
+              </p>
+            </div>
+          </div>
+          {/*Right side*/}
+          <div className="flex-1 relative overflow-hidden">
+            <img src="/device-pile.png" alt="Device image" className="mt-4 z-20 relative" />
+            <video className="absolute top-2 left-1/2 -translate-x-1/2 h-4/6 z-10 max-w-[63%]" playsInline autoPlay={true} muted loop>
+              <source src="../../../public/video-devices.m4v" type="video/mp4" />
+            </video>
+          </div>
         </div>
       </div>
     </div>
