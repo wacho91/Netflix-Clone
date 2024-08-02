d86d49f214d7391f90ab7c6c7c671f758e7538dc
wacho91 <criskol.71@gmail.com>
2024-08-02 10:46:57 -0500

    Section complete

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -69,7 +69,36 @@ const AuthScreen = () => {
       <div className="py-10 bg-black text-white">
         <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
           {/*Left side*/}
+          <div className='flex-1 relative'>
+						<div className='relative'>
+							<img src='/stranger-things-lg.png' alt='Stranger Things img' className='mt-4' />
+
+							<div
+								className='flex items-center gap-2 absolute bottom-5 left-1/2 -translate-x-1/2 bg-black
+              w-3/4 lg:w-1/2 h-24 border border-slate-500 rounded-md px-2
+              '
+							>
+								<img src='/stranger-things-sm.png' alt='image' className='h-full' />
+								<div className=' flex justify-between items-center w-full'>
+									<div className='flex flex-col gap-0'>
+										<span className='text-md lg:text-lg font-bold'>Stranger Things</span>
+										<span className='text-sm text-blue-500'>Downloading...</span>
+									</div>
+
+									<img src='/download-icon.gif' alt='' className='h-12' />
+								</div>
+							</div>
+						</div>
+					</div>
           {/*Right side*/}
+          <div className='flex-1 md:text-left text-center'>
+						<h2 className='text-4xl md:text-5xl font-extrabold mb-4 text-balance'>
+							Download your shows to watch offline
+						</h2>
+						<p className='text-lg md:text-xl'>
+							Save your favorites easily and always have something to watch.
+						</p>
+					</div>
         </div>
       </div>
     </div>
