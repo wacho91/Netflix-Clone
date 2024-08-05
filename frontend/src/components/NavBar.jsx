7955b8a11a27e30dfedbe2f9841dd92c14cbb0d9
wacho91 <criskol.71@gmail.com>
2024-08-05 11:50:16 -0500

    Navbar complete

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,34 +1,47 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import { LogOut, Menu, Search } from "lucide-react";
+import { useAuthStore } from "../store/authUser";
 
 const NavBar = () => {
 
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
+    const {user, logout} = useAuthStore()
     const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
     
 
   return (
     <header className="max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20">
-      <div className="flex items-center gap-10 z-50">
-        <Link to='/'>
-            <img src="../../public/netflix-logo.png" alt="logo" className="w-32 sm:40" />
-        </Link>
-        {/*Desktop navbar items*/}
-        <div className="hidden sm:flex gap-2 items-center">
-            <Link to='/' className="hover:underline">
-                Movies
+        <div className="flex items-center gap-10 z-50">
+            <Link to='/'>
+                <img src="../../public/netflix-logo.png" alt="logo" className="w-32 sm:40" />
             </Link>
+            {/*Desktop navbar items*/}
+            <div className="hidden sm:flex gap-2 items-center">
+                <Link to='/' className="hover:underline">
+                    Movies
+                </Link>
 
-            <Link to='/' className="hover:underline">
-                Tv Shows 
-            </Link>
+                <Link to='/' className="hover:underline">
+                    Tv Shows 
+                </Link>
 
-            <Link to='/history' className="hover:underline">
-                Search History
-            </Link>
+                <Link to='/history' className="hover:underline">
+                    Search History
+                </Link>
+            </div>
         </div>
-      </div>
+
+        <div className='flex gap-2 items-center z-50'>
+            <Link to={"/search"}>
+                <Search className='size-6 cursor-pointer' />
+            </Link>
+            <img src={user.image} alt='Avatar' className='h-8 rounded cursor-pointer' />
+            <LogOut className='size-6 cursor-pointer' onClick={logout} />
+            <div className='sm:hidden'>
+                <Menu className='size-6 cursor-pointer' onClick={toggleMobileMenu} />
+            </div>
+        </div>
 
       {/*Mobile navbar items*/}
       {isMobileMenuOpen && (
