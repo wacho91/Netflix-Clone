83a75e191a79edffbd5642cfb1a555d3c9b90422
wacho91 <criskol.71@gmail.com>
2024-08-09 10:53:34 -0500

    Complete searchHistory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { useEffect } from "react"
 import { Loader } from "lucide-react"
 import WatchPage from "./pages/WatchPage"
 import SearchPage from "./pages/SearchPage"
+import HistoryPage from "./pages/HistoryPage"
 
 function App() {
 
@@ -38,6 +39,7 @@ function App() {
         <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={"/"} /> } />
 
         <Route path="/search" element={user ? <SearchPage /> : <Navigate to={"/login"} /> } />
+        <Route path="/history" element={user ? <HistoryPage /> : <Navigate to={"/login"} /> } />
       </Routes>
       <Footer />
 
