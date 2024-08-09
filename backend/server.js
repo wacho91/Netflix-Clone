28d59bd5291f5fb689ab2180f3989f756bbd381f
wacho91 <criskol.71@gmail.com>
2024-08-09 11:28:47 -0500

    Algunos cambios

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from "cookie-parser";
+import path from "path"
 
 import authRoutes from './routes/authRoutes.js';
 import movieRoutes from './routes/movieRoutes.js';
@@ -15,6 +16,7 @@ import { protectRoute } from "./middleware/protectRoute.js";
 const app = express();
 
 const PORT = ENV_VARS.PORT
+const __dirname = path.resolve();
 
 app.use(express.json());// will allow use to parse req.body
 app.use(cookieParser());
@@ -23,6 +25,14 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
+
+if(ENV_VARS.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log("Server started at http://localhost:" + PORT);
